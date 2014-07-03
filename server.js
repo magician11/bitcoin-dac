47a8000d8ba95f22a11ddf7db85d861aed5f9f72
Andrew Golightly <andrew@golightlyplus.com>
2014-07-03 15:04:28 +0800

    server sends email on /email

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
 var express = require('express');
-var bitcoindacapp = express();
+var nodemailer = require('nodemailer');
+var fs = require('fs');
 
-bitcoindacapp.configure(function() {
-    bitcoindacapp.use(express.static(__dirname + '/public'));
+var bitcoindacApp = express();
+
+// set root of frontend
+bitcoindacApp.configure(function() {
+    bitcoindacApp.use(express.static(__dirname + '/public'));
 });
 
-bitcoindacapp.get('*', function(req, res) {
+// load config file
+var bitcoindacConfig = JSON.parse(fs.readFileSync('bitcoindac_config.json'));
+
+// api start ---------------------------------------------------------------------
+// send an email
+bitcoindacApp.get('/email', function(req, res) {
+
+    var smtpTransport = nodemailer.createTransport("SMTP",{
+        service: "Gmail",
+        auth: {
+            user: bitcoindacConfig.emailAuth.user,
+            pass: bitcoindacConfig.emailAuth.password
+        }
+    });
+
+    var mailOptions = {
+        from: "Michelle Yummy <foo@blurdybloop.com>",
+        to: "andrewgolightly11@gmail.com",
+        subject: "Hello xx",
+        html: "<b>Hey!</b><br><h1>love you</h1>"
+    }
+
+    // send mail with defined transport object
+    smtpTransport.sendMail(mailOptions, function(error, response){
+        if(error){
+            console.log(error);
+            res.send("Error: " + error.message);
+        }else{
+            console.log("Message sent: " + response.message);
+            res.send("Message sent: " + response.message);
+        }
+
+        smtpTransport.close();
+    });
+});
+
+bitcoindacApp.get('*', function(req, res) {
     res.sendfile('./public/index.html');
 });
 
+// end of api ---------------------------------------------------------------------
+
+// start the server
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-bitcoindacapp.listen( port, ipaddress, function() {
+bitcoindacApp.listen( port, ipaddress, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
