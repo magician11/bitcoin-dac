1f30a3b028974d6614b0ad12457a962fb82d23a5
Andrew Golightly <andrew@golightlyplus.com>
2014-07-03 11:27:19 +0800

    updated port and ip info for server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 var express = require('express');
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
