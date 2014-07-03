3a8cbb89213a2a20e010bfe287b38af22e5cb828
Andrew Golightly <andrew@golightlyplus.com>
2014-07-03 12:09:55 +0800

    updated structure

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var bitcoindacapp = express();
+
+bitcoindacapp.configure(function() {
+    bitcoindacapp.use(express.static(__dirname + '/public'));
+});
+
+bitcoindacapp.get('*', function(req, res) {
+    res.sendfile('./public/index.html');
+});
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-server.listen( port, ipaddress, function() {
+bitcoindacapp.listen( port, ipaddress, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
